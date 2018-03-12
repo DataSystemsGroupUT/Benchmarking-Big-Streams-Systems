aa0de75d0ed6a01604363018528cfee16da9a35c
Elkhan Shahverdi <shahverdiyev90@gmail.com>
2018-03-12 17:34:21 +0300

    installed

--- storm-benchmarks/src/main/java/storm/benchmark/AdvertisingTopology.java
+++ storm-benchmarks/src/main/java/storm/benchmark/AdvertisingTopology.java
@@ -4,19 +4,20 @@
  */
 package storm.benchmark;
 
-import backtype.storm.Config;
-import backtype.storm.LocalCluster;
-import backtype.storm.StormSubmitter;
-import backtype.storm.spout.SchemeAsMultiScheme;
-import backtype.storm.spout.SpoutOutputCollector;
-import backtype.storm.task.OutputCollector;
-import backtype.storm.task.TopologyContext;
-import backtype.storm.topology.OutputFieldsDeclarer;
-import backtype.storm.topology.TopologyBuilder;
-import backtype.storm.topology.base.BaseRichBolt;
-import backtype.storm.tuple.Fields;
-import backtype.storm.tuple.Tuple;
-import backtype.storm.tuple.Values;
+import org.apache.storm.Config;
+import org.apache.storm.LocalCluster;
+import org.apache.storm.StormSubmitter;
+import org.apache.storm.kafka.SpoutConfig;
+import org.apache.storm.spout.SchemeAsMultiScheme;
+import org.apache.storm.spout.SpoutOutputCollector;
+import org.apache.storm.task.OutputCollector;
+import org.apache.storm.task.TopologyContext;
+import org.apache.storm.topology.OutputFieldsDeclarer;
+import org.apache.storm.topology.TopologyBuilder;
+import org.apache.storm.topology.base.BaseRichBolt;
+import org.apache.storm.tuple.Fields;
+import org.apache.storm.tuple.Tuple;
+import org.apache.storm.tuple.Values;
 //import backtype.storm.utils.Utils;
 import benchmark.common.Utils;
 import benchmark.common.advertising.CampaignProcessorCommon;
@@ -29,12 +30,13 @@ import org.apache.commons.cli.CommandLineParser;
 import org.apache.commons.cli.DefaultParser;
 import org.apache.commons.cli.Options;
 import org.apache.log4j.Logger;
+import org.apache.storm.topology.base.BaseRichBolt;
 import org.json.JSONObject;
 import redis.clients.jedis.Jedis;
-import storm.kafka.KafkaSpout;
-import storm.kafka.SpoutConfig;
-import storm.kafka.StringScheme;
-import storm.kafka.ZkHosts;
+import org.apache.storm.kafka.KafkaSpout;
+import org.apache.storm.kafka.SpoutConfig;
+import org.apache.storm.kafka.StringScheme;
+import org.apache.storm.kafka.ZkHosts;
 
 /**
  * This is a basic example of a Storm topology.
@@ -243,7 +245,7 @@ public class AdvertisingTopology {
 
             LocalCluster cluster = new LocalCluster();
             cluster.submitTopology("test", conf, builder.createTopology());
-            backtype.storm.utils.Utils.sleep(10000);
+            org.apache.storm.utils.Utils.sleep(10000);
             cluster.killTopology("test");
             cluster.shutdown();
         }
