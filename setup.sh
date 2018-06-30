44d10626721254365e15dceaa4490786758bf7b9
Elkhan Shahverdi <shahverdiyev90@gmail.com>
2018-06-30 16:04:04 +0300

    Automatic push message

--- setup.sh
+++ setup.sh
@@ -45,46 +45,49 @@ git remote set-url origin git@bitbucket.org:elkhan_shahverdi/stream-benchmarking
 git reset --hard HEAD
 git pull
 
-#Spark
-./sbin/start-master.sh -h stream-node01 -p 7077
-./sbin/start-slave.sh spark://stream-node01:7077
-
-
-#Run zookeeper
-./bin/zookeeper-server-start.sh config/zookeeper.properties
-./bin/zookeeper-server-start.sh -daemon config/zookeeper.properties;
-
-#Run kafka server
-./bin/kafka-server-start.sh config/server.properties
-./bin/kafka-server-start.sh -daemon config/server.properties;tail -100f logs/kafkaServer.out
-./bin/kafka-server-stop.sh
-#List kafka topic
-./bin/kafka-topics.sh --list --zookeeper zookeeper-node01:2181,zookeeper-node02:2181,zookeeper-node03:2181
-
-
-#Create Kafka topic
-./bin/kafka-topics.sh --delete --zookeeper zookeeper-node01:2181,zookeeper-node02:2181,zookeeper-node03:2181 --topic ad-events
-./bin/kafka-topics.sh --create --zookeeper zookeeper-node01:2181,zookeeper-node02:2181,zookeeper-node03:2181 --replication-factor 1 --partitions 4 --topic ad-events
-./bin/kafka-topics.sh --create --zookeeper zookeeper-node01:2181,zookeeper-node02:2181,zookeeper-node03:2181 --replication-factor 1 --partitions 4 --topic sample-test
-
-./bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 2 --topic ad-events
-
-#Producer
-./bin/kafka-console-producer.sh --broker-list  kafka-node01:9092,kafka-node02:9092,kafka-node03:9092,kafka-node04:9092 --topic sample-test
-
-#Consumer
-./bin/kafka-console-consumer.sh --zookeeper zookeeper-node01:2181,zookeeper-node02:2181,zookeeper-node03:2181 --topic sample-test --from-beginning
-
-
-
-scp apache-storm-1.2.1/conf/storm.yaml ubuntu@stream-node01:~/stream-benchmarking/apache-storm-1.2.1/conf/storm.yaml
-scp apache-storm-1.2.1/conf/storm.yaml ubuntu@stream-node02:~/stream-benchmarking/apache-storm-1.2.1/conf/storm.yaml
-scp apache-storm-1.2.1/conf/storm.yaml ubuntu@stream-node03:~/stream-benchmarking/apache-storm-1.2.1/conf/storm.yaml
-scp apache-storm-1.2.1/conf/storm.yaml ubuntu@stream-node04:~/stream-benchmarking/apache-storm-1.2.1/conf/storm.yaml
-scp apache-storm-1.2.1/conf/storm.yaml ubuntu@stream-node05:~/stream-benchmarking/apache-storm-1.2.1/conf/storm.yaml
-scp apache-storm-1.2.1/conf/storm.yaml ubuntu@stream-node06:~/stream-benchmarking/apache-storm-1.2.1/conf/storm.yaml
-scp apache-storm-1.2.1/conf/storm.yaml ubuntu@stream-node07:~/stream-benchmarking/apache-storm-1.2.1/conf/storm.yaml
-scp apache-storm-1.2.1/conf/storm.yaml ubuntu@stream-node08:~/stream-benchmarking/apache-storm-1.2.1/conf/storm.yaml
+##Spark
+#./sbin/start-master.sh -h stream-node01 -p 7077
+#./sbin/start-slave.sh spark://stream-node01:7077
+#
+#
+##Run zookeeper
+#./bin/zookeeper-server-start.sh config/zookeeper.properties
+#./bin/zookeeper-server-start.sh -daemon config/zookeeper.properties;
+#
+##Run kafka server
+#./bin/kafka-server-start.sh config/server.properties
+#./bin/kafka-server-start.sh -daemon config/server.properties;tail -100f logs/kafkaServer.out
+#./bin/kafka-server-stop.sh
+##List kafka topic
+#./bin/kafka-topics.sh --list --zookeeper zookeeper-node01:2181,zookeeper-node02:2181,zookeeper-node03:2181
+#
+#
+##Create Kafka topic
+#./bin/kafka-topics.sh --delete --zookeeper zookeeper-node01:2181,zookeeper-node02:2181,zookeeper-node03:2181 --topic ad-events
+#./bin/kafka-topics.sh --create --zookeeper zookeeper-node01:2181,zookeeper-node02:2181,zookeeper-node03:2181 --replication-factor 1 --partitions 4 --topic ad-events
+#./bin/kafka-topics.sh --create --zookeeper zookeeper-node01:2181,zookeeper-node02:2181,zookeeper-node03:2181 --replication-factor 1 --partitions 4 --topic sample-test
+#
+#./bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 2 --topic ad-events
+#
+##Producer
+#./bin/kafka-console-producer.sh --broker-list  kafka-node01:9092,kafka-node02:9092,kafka-node03:9092,kafka-node04:9092 --topic sample-test
+#
+##Consumer
+#./bin/kafka-console-consumer.sh --zookeeper zookeeper-node01:2181,zookeeper-node02:2181,zookeeper-node03:2181 --topic sample-test --from-beginning
+#
+#
+#
+#scp apache-storm-1.2.1/conf/storm.yaml ubuntu@stream-node01:~/stream-benchmarking/apache-storm-1.2.1/conf/storm.yaml
+#scp apache-storm-1.2.1/conf/storm.yaml ubuntu@stream-node02:~/stream-benchmarking/apache-storm-1.2.1/conf/storm.yaml
+#scp apache-storm-1.2.1/conf/storm.yaml ubuntu@stream-node03:~/stream-benchmarking/apache-storm-1.2.1/conf/storm.yaml
+#scp apache-storm-1.2.1/conf/storm.yaml ubuntu@stream-node04:~/stream-benchmarking/apache-storm-1.2.1/conf/storm.yaml
+#scp apache-storm-1.2.1/conf/storm.yaml ubuntu@stream-node05:~/stream-benchmarking/apache-storm-1.2.1/conf/storm.yaml
+#scp apache-storm-1.2.1/conf/storm.yaml ubuntu@stream-node06:~/stream-benchmarking/apache-storm-1.2.1/conf/storm.yaml
+#scp apache-storm-1.2.1/conf/storm.yaml ubuntu@stream-node07:~/stream-benchmarking/apache-storm-1.2.1/conf/storm.yaml
+#scp apache-storm-1.2.1/conf/storm.yaml ubuntu@stream-node08:~/stream-benchmarking/apache-storm-1.2.1/conf/storm.yaml
+#scp apache-storm-1.2.1/conf/storm.yaml ubuntu@stream-node09:~/stream-benchmarking/apache-storm-1.2.1/conf/storm.yaml
+#scp apache-storm-1.2.1/conf/storm.yaml ubuntu@stream-node10:~/stream-benchmarking/apache-storm-1.2.1/conf/storm.yaml
+#scp apache-storm-1.2.1/conf/storm.yaml ubuntu@stream-node11:~/stream-benchmarking/apache-storm-1.2.1/conf/storm.yaml
 
 
 
