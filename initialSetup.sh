24d90472bafa85cc37eba02e92312c8aed05658c
Elkhan Shahverdi <shahverdiyev90@gmail.com>
2018-06-30 16:01:41 +0300

    Automatic push message

---
+++ initialSetup.sh
@@ -0,0 +1,35 @@
+#! /usr/bin/env bash
+ssh-keygen -t rsa -P ""
+sudo apt-get upgrade
+sudo add-apt-repository ppa:webupd8team/java 
+sudo apt-get update
+sudo apt-get install oracle-java8-installer
+sudo apt-get install maven
+sudo update-alternatives --config java
+
+
+source /etc/environment
+echo $JAVA_HOME
+
+sudo apt-get install make
+sudo apt-get install gcc
+sudo apt-get install tcl
+sudo apt-get install build-essential
+
+sudo add-apt-repository ppa:deadsnakes/ppa
+sudo apt-get update
+sudo apt-get install python2.7
+
+wget -O- https://raw.githubusercontent.com/nicolargo/glancesautoinstall/master/install.sh | sudo /bin/bash
+
+
+wget https://raw.githubusercontent.com/technomancy/leiningen/stable/bin/lein
+sudo mkdir -p /usr/local/bin/
+sudo mv ./lein* /usr/local/bin/lein
+sudo chmod a+x /usr/local/bin/lein
+export PATH=$PATH:/usr/local/bin
+lein repl
+
+sudo apt-get update
+sudo apt-get install git
+sudo reboot
--- setup.sh
+++ setup.sh
@@ -1,41 +1,9 @@
 #! /usr/bin/env bash
-sudo apt-get upgrade
-sudo add-apt-repository ppa:webupd8team/java 
-sudo apt-get update
-sudo apt-get install oracle-java8-installer
-sudo apt-get install maven
-sudo update-alternatives --config java
 
-
-source /etc/environment
-echo $JAVA_HOME
-
-sudo apt-get install make
-sudo apt-get install gcc
-sudo apt-get install tcl
-sudo apt-get install build-essential
-
-sudo add-apt-repository ppa:deadsnakes/ppa
-sudo apt-get update
-sudo apt-get install python2.7
-
-wget -O- https://raw.githubusercontent.com/nicolargo/glancesautoinstall/master/install.sh | sudo /bin/bash
-
-
-wget https://raw.githubusercontent.com/technomancy/leiningen/stable/bin/lein
-sudo mkdir -p /usr/local/bin/
-sudo mv ./lein* /usr/local/bin/lein
-sudo chmod a+x /usr/local/bin/lein
-export PATH=$PATH:/usr/local/bin
-lein repl
-
-ssh-keygen -t rsa -P ""
-sudo apt-get update
-sudo apt-get install git
 git clone git@bitbucket.org:elkhan_shahverdi/stream-benchmarking.git
 cd stream-benchmarking
 ./stream-bench.sh SETUP
-sudo reboot
+
 
 
 sed -i 's/taskmanager.heap.mb: 1024/taskmanager.heap.mb: 6144/g' /home/ubuntu/stream-benchmarking/flink-1.4.0/conf/flink-conf.yaml
