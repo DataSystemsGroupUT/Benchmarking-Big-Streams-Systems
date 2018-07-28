636bb265c78fba1ccc875ea729321e5376daa7ee
Elkhan Shahverdi <shahverdiyev90@gmail.com>
2018-07-28 19:17:46 +0300

    going to test kafka

--- BenchmarkResult.r
+++ BenchmarkResult.r
@@ -4,21 +4,36 @@
 ##########################                             Stream Benchmark Result                              ##########################
 ##########################                                                                                  ##########################
 ######################################################################################################################################
+#!/usr/bin/env Rscript
+library(ggplot2)
+library(scales)
+theme_set(theme_bw())
+options("scipen"=10)
+args <- commandArgs(TRUE)
+tps <- as.numeric(args[2])
+duration <- as.numeric(args[3])
 
-generateBenchmarkReport <- function(engine, tps, duration){
+trim <- function (x) gsub("^\\s+|\\s+$", "", x)
+
+
+#generateBenchmarkReport <- function(engine, tps, duration){
   result = NULL
+  engine="flink"
+  tps=1000
+  duration="600"
   for(i in 1:10) {
-    
+    i = 1  
     TPS = toString(tps * i)
     reportFolder = paste("/Users/sahverdiyev/Desktop/EDU/THESIS/stream-benchmarking/result/", engine, "/", sep = "")
     sourceFolder = paste("/Users/sahverdiyev/Desktop/EDU/THESIS/stream-benchmarking/result/", engine, "/TPS_", TPS,"_DURATION_",toString(duration),"/", sep = "")
     Seen = read.table(paste(sourceFolder, "redis-seen.txt",sep=""),header=F,stringsAsFactors=F,sep=',')
     Updated = read.table(paste(sourceFolder, "redis-updated.txt",sep=""),header=F,stringsAsFactors=F,sep=',')
-    
-    SumProceed = c()
-    for(c in 1:length(Seen$V1)) {
-      SumProceed[c] = sum(Seen$V1[1:c])
+
+    windows = c()
+    for(c in 1:length(Updated$V1)) {
+      windows[c] = c
     }
+    #df <- data.frame(toString(tps*i*10), Seen$V1, Updated$V1 - 10000, round(SumProceed*100/sum(Seen$V1),digits=0))
     df <- data.frame(toString(tps*i*10), Seen$V1, Updated$V1 - 10000, round(SumProceed*100/sum(Seen$V1),digits=0))
     result <- rbind(result, df)
     
@@ -27,7 +42,8 @@ generateBenchmarkReport <- function(engine, tps, duration){
     }
 
     names(df) <- c("TPS","Seen","Throughput", "Percentile")
-    df = df[df$Throughput > 0,]
+    #df = df[df$Throughput > 0,]
+    df
     ggplot(data=df, aes(x=Percentile, y=Throughput, group=TPS, colour=TPS)) + 
       geom_smooth(method="auto", se=F) + 
       guides(fill=FALSE) +
@@ -46,4 +62,4 @@ generateBenchmarkReport <- function(engine, tps, duration){
     theme(plot.title = element_text(size = 13, face = "plain"), text = element_text(size = 12, face = "plain"))
   ggsave(paste(engine,"_", duration, ".pdf", sep=""), width = 20, height = 20, units = "cm", device = "pdf", path = reportFolder)
 
-}
+#}
