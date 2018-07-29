5032ff775aa2d2a3dda4493e9bd63cb106e6bc78
Elkhan Shahverdi <shahverdiyev90@gmail.com>
2018-07-29 12:58:41 +0300

    going to find spark dstream bottleneck

--- reporting/BenchmarkPercentile.R
+++ reporting/BenchmarkPercentile.R
@@ -14,11 +14,56 @@ tps <- as.numeric(args[2])
 duration <- as.numeric(args[3])
 
 trim <- function (x) gsub("^\\s+|\\s+$", "", x)
-
-engine="flink"
+engines <- c("flink", "kafka")
 tps=1000
 duration=600
+percentile=95
+eng=1
 i=1
+generateBenchmarkSpesificPercentile <- function(engines, tps, duration, percentile){
+  result = NULL
+  for(eng in 1:length(engines)){
+    engine = engines[eng] 
+    for(i in 1:15) {
+      TPS = toString(tps * i)
+      TPS
+      reportFolder = paste("/Users/sahverdiyev/Desktop/EDU/THESIS/stream-benchmarking/result/", engine, "/", sep = "")
+      sourceFolder = paste("/Users/sahverdiyev/Desktop/EDU/THESIS/stream-benchmarking/result/", engine, "/TPS_", TPS,"_DURATION_",toString(duration),"/", sep = "")
+      Seen = read.table(paste(sourceFolder, "redis-seen.txt",sep=""),header=F,stringsAsFactors=F,sep=',')
+      Updated = read.table(paste(sourceFolder, "redis-updated.txt",sep=""),header=F,stringsAsFactors=F,sep=',')
+      
+      windows = c()
+      SeenFiltered = c()
+      UpdatedFiltered = c()
+      for(c in 1:(length(Updated$V1)-1)) {
+        if(Seen$V1[c] != Seen$V1[c+1] && Updated$V1[c] != Updated$V1[c+1] && Updated$V1[c] > 10000){
+          SeenFiltered <- c(SeenFiltered, Seen$V1[c])
+          UpdatedFiltered <- c(UpdatedFiltered, Updated$V1[c])
+        }
+      }
+      UpdatedFiltered <- sort(UpdatedFiltered)
+      df <- data.frame(tps*i*10, engine, UpdatedFiltered[round(percentile/100*(length(UpdatedFiltered)+1))]-10000)
+      
+      result <- rbind(result, df)
+      df
+      if (length(Seen$V1)  != length(Updated$V1)){ 
+        stop("Input data set is wrong. Be sure you have selected correct collections")
+      }
+      names(df) <- c("TPS","Engine","Throughput")
+    }
+  }
+  names(result) <- c("TPS","Engine","Throughput")
+  ggplot(data=result, aes(x=TPS, y=Throughput, group=Engine, colour=Engine)) + 
+    geom_point() + geom_smooth(method="loess", se=F) +
+    guides(fill=FALSE) +
+    scale_x_continuous(breaks = round(seq(min(result$TPS), max(result$TPS), by = 10000),1)) +
+    scale_y_continuous(breaks = round(seq(min(result$Throughput), max(result$Throughput), by = 1000),1)) +
+    xlab("Througput (event/s)") + ylab("Window Latency ms ") +
+    ggtitle(paste(toupper(engine), toString(percentile), "% Percentile chart", sep = " ")) +
+    theme(plot.title = element_text(size = 13, face = "plain"), axis.text.x = element_text(angle = 30, hjust = 1), text = element_text(size = 12, face = "plain"))
+  ggsave(paste(engine,"_", duration, "_percentile.pdf", sep=""), width = 20, height = 20, units = "cm", device = "pdf", path = reportFolder)
+}
+
 generateBenchmarkPercentile <- function(engine, tps, duration){
   result = NULL
   for(i in 1:15) {
@@ -33,7 +78,7 @@ generateBenchmarkPercentile <- function(engine, tps, duration){
     UpdatedFiltered = c()
     percentile = c()
     for(c in 1:(length(Updated$V1)-1)) {
-      if(Seen$V1[c] != Seen$V1[c+1] && Updated$V1[c] != Updated$V1[c+1]){
+      if(Seen$V1[c] != Seen$V1[c+1] && Updated$V1[c] != Updated$V1[c+1] && Updated$V1[c] > 10000){
         SeenFiltered <- c(SeenFiltered, Seen$V1[c])
         UpdatedFiltered <- c(UpdatedFiltered, Updated$V1[c])
       }
@@ -59,7 +104,7 @@ generateBenchmarkPercentile <- function(engine, tps, duration){
     geom_smooth(method="loess", se=F) + 
     guides(fill=FALSE) +
     xlab("Percentage of Completed Tuple") + ylab("Window Throughput ms ") +
-    ggtitle(paste(toupper(engine), "Benchmark", sep = " ")) +
+    ggtitle(paste(toupper(engine), "Benchmark Percentile chart", sep = " ")) +
     theme(plot.title = element_text(size = 13, face = "plain"), text = element_text(size = 12, face = "plain"))
   ggsave(paste(engine,"_", duration, "_percentile.pdf", sep=""), width = 20, height = 20, units = "cm", device = "pdf", path = reportFolder)
 }
--- reporting/reporting.r
+++ reporting/reporting.r
@@ -6,16 +6,16 @@ options("scipen"=10)
 args <- commandArgs(TRUE)
 tps <- as.numeric(args[2])
 duration <- as.numeric(args[3])
-
+engines <- c("flink","spark_dataset_3000","spark_dstream_3000", "kafka", "storm")
 source('~/Desktop/EDU/THESIS/stream-benchmarking/reporting/StreamServerReport.r')
 source('~/Desktop/EDU/THESIS/stream-benchmarking/reporting/KafkaServerReport.r')
 source('~/Desktop/EDU/THESIS/stream-benchmarking/reporting/BenchmarkResult.r')
 source('~/Desktop/EDU/THESIS/stream-benchmarking/reporting/BenchmarkPercentile.R')
+generateBenchmarkSpesificPercentile(engines, 1000, 600, 95)  
 generateBenchmarkReport("flink", 1000, 600)
 generateStreamServerLoadReport("flink", 1000, 600)
 generateKafkaServerLoadReport("flink", 1000, 600)
-generateBenchmarkPercentile("flink", 1000, 60)
-  
+generateBenchmarkPercentile("flink", 1000, 600)
 
 
 trim <- function (x) gsub("^\\s+|\\s+$", "", x)
@@ -26,27 +26,11 @@ generateKafkaServerLoadReport(args[1], tps, duration)
 
 
 if(length(args) == 0){
-  generateBenchmarkReport("kafka", 1000, 600)
-  generateStreamServerLoadReport("kafka", 1000, 600)
-  generateKafkaServerLoadReport("kafka", 1000, 600)
-
-
-  generateBenchmarkReport("flink", 1000, 600)
-  generateStreamServerLoadReport("flink", 1000, 600)
-  generateKafkaServerLoadReport("flink", 1000, 600)
-  
-  generateBenchmarkReport("spark_dstream_3000", 1000, 600)
-  generateStreamServerLoadReport("spark_dstream_3000", 1000, 600)
-  generateKafkaServerLoadReport("spark_dstream_3000", 1000, 600)
-  
-  generateBenchmarkReport("spark_dataset_3000", 1000, 600)
-  generateStreamServerLoadReport("spark_dataset_3000", 1000, 600)
-  generateKafkaServerLoadReport("spark_dataset_3000", 1000, 600)
-  
-  generateBenchmarkReport("storm_with_ack", 1000, 600)
-  generateStreamServerLoadReport("storm_with_ack", 1000, 600)
-  generateKafkaServerLoadReport("storm_with_ack", 1000, 600)
-
+  for (i in 1:length(engines)) { 
+    generateBenchmarkReport(engines[i], 1000, 600)
+    generateStreamServerLoadReport(engines[i], 1000, 600)
+    generateKafkaServerLoadReport(engines[i], 1000, 600)
+  }
 }
   
 
