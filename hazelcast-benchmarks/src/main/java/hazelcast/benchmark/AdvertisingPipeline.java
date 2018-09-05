ec93565d72918cf75e7789f52969eddd37860b03
Elkhan Shahverdi <shahverdiyev90@gmail.com>
2018-09-05 15:54:38 +0300

    Going to test Hazelcast JET with different version

--- hazelcast-benchmarks/src/main/java/hazelcast/benchmark/AdvertisingPipeline.java
+++ hazelcast-benchmarks/src/main/java/hazelcast/benchmark/AdvertisingPipeline.java
@@ -128,7 +128,7 @@ public class AdvertisingPipeline {
         Pipeline pipeline = Pipeline.create();
         pipeline
                 .drawFrom(KafkaSources.kafka(properties, kafkaTopic))
-                .setLocalParallelism(kafkaPartitions)
+                //.setLocalParallelism(kafkaPartitions)
                 .map(objectObjectEntry -> deserializeBolt(objectObjectEntry.getValue().toString()))
                 .setLocalParallelism(parallel)
                 .filter(tuple -> tuple._5().equals("view"))
@@ -138,7 +138,7 @@ public class AdvertisingPipeline {
                 .customTransform("test2", () -> new RedisJoinBoltP(redisServerHost))
                 .setLocalParallelism(parallel)
                 .customTransform("test3", () -> new WriteRedisBoltP(redisServerHost, timeDivisor))
-                .setLocalParallelism(parallel)
+                .setLocalParallelism(1)
 
 
                 //.addTimestamps(AdsEnriched::getEvent_time, TimeUnit.SECONDS.toMillis(0))
